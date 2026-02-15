47cbbf4774b1cedacc2b780db04236dddef84004
Daniil <daniil@example.com>
2026-02-15 19:38:51 +0300

    Homework 15
    Доработка консольного сетевого чата
    Добавлена команда /w для личных сообщений
    Формат: /w nick сообщение

---
+++ homework-15/server/src/main/java/ru/otus/chat/server/ServerApp.java
@@ -0,0 +1,9 @@
+package ru.otus.chat.server;
+
+public class ServerApp {
+    public static final int PORT = 8189;
+
+    public static void main(String[] args) {
+        new Server(PORT).start();
+    }
+}
